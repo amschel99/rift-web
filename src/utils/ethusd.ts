6fc949aa5a76192af4ae6fa845a6ab10bd3856d2
mrgustaveh <mburuantony@outlook.com>
2024-12-22 17:51:17 +0300

    updates & fixes - btc -usd -shared secrets

--- src/utils/ethusd.ts
+++ src/utils/ethusd.ts
@@ -1,6 +1,34 @@
+type btcUSD = {
+  bitcoin: {
+    usd: number;
+  };
+};
+
 type ethUSD = {
   ethereum: {
     usd: number;
+  };
+};
+
+export const getBtcUsdVal = async (btcQty: number) => {
+  const APIURL =
+    "https://pro-api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&x_cg_pro_api_key=CG-Whw1meTdSTTT7CSpGZbaB3Yi";
+
+  let res: Response = await fetch(APIURL, {
+    method: "GET",
+
+    headers: {
+      accept: "application/json",
+    },
+  });
+  let data: btcUSD = await res.json();
+
+  let btcToUsd: number = data?.bitcoin?.usd;
+
+  return {
+    btcQtyInUSD: (btcQty as number) * btcToUsd,
+    btcValue: data?.bitcoin?.usd,
+    success: res.ok,
   };
 };
 
