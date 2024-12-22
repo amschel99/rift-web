f5b115ae9821d3a62e8a2935f2c1ba79963e5364
mrgustaveh <mburuantony@outlook.com>
2024-12-22 17:49:34 +0300

    updates & fixes - btc -usd -shared secrets

--- src/components/WalletBalance.tsx
+++ src/components/WalletBalance.tsx
@@ -6,8 +6,11 @@ import {
   useEffect,
   useState,
 } from "react";
-import { walletBalance } from "../utils/api/wallet";
-import { getEthUsdVal } from "../utils/ethusd";
+import { useNavigate } from "react-router";
+import { walletBalance, uSdTBalance } from "../utils/api/wallet";
+import { getBtcUsdVal, getEthUsdVal } from "../utils/ethusd";
+import { ArrowRight } from "../assets/icons";
+import { colors } from "../constants";
 import "../styles/components/walletbalance.css";
 
 interface props {
@@ -19,8 +22,13 @@ export const WalletBalance = ({
   refreshing,
   setRefreshing,
 }: props): JSX.Element => {
+  const navigate = useNavigate();
+
   const [accBalLoading, setAccBalLoading] = useState<boolean>(false);
-  const [accBalance, setAccBalance] = useState<number | undefined>(undefined);
+  const [accBalance, setAccBalance] = useState<number>(0);
+  const [btcAccBalance, setBtcAccAccBalance] = useState<number>(0);
+  const [btcAccBalanceUsd, setBtcAccAccBalanceUsd] = useState<number>(0);
+  const [usdtAccBalance, setusdtAccBalance] = useState<number>(0);
   const [amountInUsd, setAmountInUsd] = useState<number>(0);
   const [geckoSuccess, setGeckoSuccess] = useState<boolean>(false);
 
@@ -29,13 +37,18 @@ export const WalletBalance = ({
 
     let access: string | null = localStorage.getItem("token");
 
-    const { balance } = await walletBalance(access as string);
+    const { btcBalance, balance } = await walletBalance(access as string);
     const { ethInUSD, success } = await getEthUsdVal(Number(balance));
+    const { btcQtyInUSD } = await getBtcUsdVal(Number(btcBalance));
+    const { data } = await uSdTBalance(access as string);
 
     setAccBalance(Number(balance));
-    setAccBalLoading(false);
-
+    setBtcAccAccBalance(btcBalance);
+    setBtcAccAccBalanceUsd(btcQtyInUSD);
+    setusdtAccBalance(Number(data?.balance));
     setAmountInUsd(ethInUSD);
+
+    setAccBalLoading(false);
     setGeckoSuccess(success);
     setRefreshing(false);
   }, []);
@@ -57,17 +70,23 @@ export const WalletBalance = ({
       <p className="bal">Your Balance</p>
 
       <p className="balinusd">
-        {geckoSuccess ? `${usdFormatter.format(amountInUsd)}` : "- - -"}
-        {/* <span>{accBalLoading ? "- - -" : `${accBalance?.toFixed(8)} ETH`}</span> */}
+        {geckoSuccess
+          ? `${usdFormatter.format(
+              btcAccBalanceUsd + amountInUsd + usdtAccBalance
+            )}`
+          : "- - -"}
       </p>
-      <p className="_asset">
-        {accBalLoading ? "- - -" : `${accBalance?.toFixed(8)} BTC`}
+      <p className="_asset" onClick={() => navigate("/btc-asset")}>
+        {accBalLoading ? "- - -" : `${btcAccBalance?.toFixed(8)} BTC`}
+        <ArrowRight color={colors.textsecondary} />
       </p>
-      <p className="_asset">
+      <p className="_asset" onClick={() => navigate("/eth-asset")}>
         {accBalLoading ? "- - -" : `${accBalance?.toFixed(8)} ETH`}
+        <ArrowRight color={colors.textsecondary} />
       </p>
-      <p className="_asset">
-        {accBalLoading ? "- - -" : `${accBalance?.toFixed(8)} USDT`}
+      <p className="_asset _l_asset" onClick={() => navigate("/usdt-asset")}>
+        {accBalLoading ? "- - -" : `${usdtAccBalance?.toFixed(8)} USDT`}
+        <ArrowRight color={colors.textsecondary} />
       </p>
     </div>
   );
