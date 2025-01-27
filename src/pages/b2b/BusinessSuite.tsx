95b068c0f9d3c2d4c2814fb7667fdf715676dee9
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-01-27 13:07:32 +0300

    update: add utility functions and new components for enhanced UI and charting features

--- src/pages/b2b/BusinessSuite.tsx
+++ src/pages/b2b/BusinessSuite.tsx
@@ -5,8 +5,10 @@ import {
   IconArrowBackUp,
   IconChartInfographic,
   IconHomeFilled,
+  IconPlus,
   IconUserCircle,
 } from "@tabler/icons-react";
+import { Airdropped } from "../../components/charts/Airdropped";
 
 function BusinessSuite() {
   toast.success("Switched to Sphere Business");
@@ -22,6 +24,27 @@ function BusinessSuite() {
       <p className="text-sm text-textsecondary text-center leading-relaxed pb-2 border-b-[1px] border-gray-500">
         Streamline Airdrops and Token Distributions with One Click
       </p>
+      <div className=" px-2">
+        <div className="w-full bg-[#fe4a22] rounded-xl mt-2 h-auto">
+          <div className="flex items-center justify-between">
+            <div className="px-4 py-4">
+              <h1 className="text-textprimary text-4xl font-body font-bold">
+                $1,000
+              </h1>
+              <p className="text-gray-600 text-xs font-semibold leading-relaxed">
+                Airdropped this month
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center pr-2">
+              <IconPlus size={30} />
+              {/* <p className="text-gray-600 text-xs font-semibold leading-relaxed">
+                Create
+              </p> */}
+            </div>
+          </div>
+          <Airdropped />
+        </div>
+      </div>
       <div className="absolute bottom-4 w-full px-4 items-center justify-between flex bg-primary/50">
         <IconHomeFilled size={26} />
         <IconChartInfographic size={26} />
