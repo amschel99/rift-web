35623a539eab38c8ba9d734f844b089cdf79d88a
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-01-27 17:56:57 +0300

    update: implement back button functionality and improve layout in Business Suite page

--- src/pages/b2b/BusinessSuite.tsx
+++ src/pages/b2b/BusinessSuite.tsx
@@ -1,37 +1,52 @@
 import { useEffect } from "react";
 import toast from "react-hot-toast";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 import {
-  IconArrowBackUp,
   IconChartInfographic,
   IconHomeFilled,
   IconPlus,
   IconUserCircle,
 } from "@tabler/icons-react";
 import { Airdropped } from "../../components/charts/Airdropped";
+import { backButton } from "@telegram-apps/sdk-react";
 
 function BusinessSuite() {
+  const navigate = useNavigate();
+  const backbuttonclick = () => {
+    navigate(-1);
+  };
   toast.success("Switched to Sphere Business");
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (backButton.isSupported()) {
+      backButton.mount();
+      backButton.show();
+    }
+
+    if (backButton.isMounted()) {
+      backButton.onClick(backbuttonclick);
+    }
+
+    return () => {
+      backButton.unmount();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="h-screen bg-primary font-body relative">
-      <Link to="/app">
-        <IconArrowBackUp size={26} />
-      </Link>
-      <h1 className="text-textprimary text-xl font-bold text-center">
+      <h1 className="text-textprimary text-xl font-bold text-center mt-1">
         Business One Click Suite
       </h1>
       <p className="text-sm text-textsecondary text-center leading-relaxed pb-2 border-b-[1px] border-gray-500">
         Streamline Airdrops and Token Distributions with One Click
       </p>
       <div className=" px-2">
-        <div className="w-full bg-[#fe4a22] rounded-xl mt-2 h-auto">
+        <div className="w-full bg-divider rounded-xl mt-2 h-auto">
           <div className="flex items-center justify-between">
             <div className="px-4 py-4">
               <h1 className="text-textprimary text-4xl font-body font-bold">
                 $1,000
               </h1>
-              <p className="text-gray-600 text-xs font-semibold leading-relaxed">
+              <p className="text-gray-500 text-xs font-semibold leading-relaxed">
                 Airdropped this month
               </p>
             </div>
