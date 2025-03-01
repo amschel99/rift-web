1a7014589b70e50f51557f58a34cb6a6af9adb0d
mrgustaveh <mburuantony@outlook.com>
2025-03-01 13:51:30 +0300

    refactor: radio button

---
+++ src/components/global/Radios.tsx
@@ -0,0 +1,34 @@
+import { JSX } from "react";
+import { colors } from "../../constants";
+import "../../styles/components/global/radios.scss";
+
+interface props {
+  title: string;
+  description: string;
+  ischecked: boolean;
+  onclick: () => void;
+}
+
+export const RadioButton = ({
+  title,
+  description,
+  ischecked,
+  onclick,
+}: props): JSX.Element => {
+  return (
+    <div className="radio_btn_ctr" onClick={onclick}>
+      <div className="radio_ctr">
+        <div
+          style={{
+            backgroundColor: ischecked ? colors.textprimary : colors.primary,
+          }}
+        />
+      </div>
+
+      <p className="text_alt_text">
+        {title}
+        <span>{description}</span>
+      </p>
+    </div>
+  );
+};
