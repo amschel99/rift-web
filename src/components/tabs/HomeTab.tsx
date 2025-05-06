c7f1f912c69fed4a5957241977790ae58c842ce7
mrgustaveh <mburuantony@outlook.com>
2025-05-07 00:00:09 +0300

    updated hometab

--- src/components/tabs/HomeTab.tsx
+++ src/components/tabs/HomeTab.tsx
@@ -1,5 +1,261 @@
-import { JSX } from "react";
+import { Fragment, JSX, useEffect } from "react";
+import { backButton } from "@telegram-apps/sdk-react";
+import { useQuery } from "@tanstack/react-query";
+import { AppBar } from "../AppBar";
+import { AssetBalance, WalletAction, WalletBalance } from "../WalletBalance";
+import { ArrowDownCircle, ArrowUpCircle, PlusSolid } from "../../assets/icons";
+import {
+  getEthBalance,
+  getBeraBalance,
+  getPolygonUsdBalance,
+  getBeraUsdcBalance,
+} from "../../utils/api/wallet";
+import { getUnlockedTokens } from "../../utils/api/airdrop";
+import { fetchCoinInfo } from "../../utils/coingecko/markets";
+import { getSphrUsdcRate } from "../../utils/api/sphere";
+import { colors } from "../../constants";
+import "../../styles/components/tabs/hometab.scss";
 
 export const HomeTab = (): JSX.Element => {
-  return <div id="hometab">Home tab here</div>;
+  const { data: ethbalance, isFetching: ethbalfetching } = useQuery({
+    queryKey: ["ethbalance"],
+    queryFn: getEthBalance,
+  });
+
+  const { data: berabalance, isFetching: berabalfetching } = useQuery({
+    queryKey: ["berabalance"],
+    queryFn: getBeraBalance,
+  });
+
+  const { data: polygonusdcbalance, isFetching: polygonusdcfetching } =
+    useQuery({
+      queryKey: ["polygonusdcbalance"],
+      queryFn: getPolygonUsdBalance,
+    });
+
+  const { data: berausdcbalance, isFetching: berausdcbalfetching } = useQuery({
+    queryKey: ["berausdcbalance"],
+    queryFn: getBeraUsdcBalance,
+  });
+
+  const { data: unlockedtokens, isFetching: unlockedtokensfetching } = useQuery(
+    {
+      queryKey: ["unlockedTokens"],
+      queryFn: getUnlockedTokens,
+    }
+  );
+  //
+  const { data: ethereumInfo, isFetching: ethinfofetching } = useQuery({
+    queryKey: ["ethinfo"],
+    queryFn: () => fetchCoinInfo("ethereum"),
+  });
+
+  const { data: usdcInfo, isFetching: usdcinfofetching } = useQuery({
+    queryKey: ["usdcinfo"],
+    queryFn: () => fetchCoinInfo("usd-coin"),
+  });
+
+  const { data: beraInfo, isFetching: berainfofetching } = useQuery({
+    queryKey: ["berachainbera"],
+    queryFn: () => fetchCoinInfo("berachain-bera"),
+  });
+  //
+  const { data: sphereusdcrate, isFetching: sphereusdcfetching } = useQuery({
+    queryKey: ["sphereusdcrate"],
+    queryFn: getSphrUsdcRate,
+  });
+
+  const berabalUsd =
+    Number(berabalance?.data?.balance) *
+    Number(beraInfo?.market_data?.current_price?.usd);
+  const ethbalUsd =
+    Number(ethbalance?.balance) *
+    Number(ethereumInfo?.market_data?.current_price?.usd);
+
+  localStorage.setItem("berabal", String(berabalance?.data?.balance));
+  localStorage.setItem("berabalusd", String(berabalUsd));
+  localStorage.setItem(
+    "berapriceusd",
+    String(beraInfo?.market_data?.current_price?.usd)
+  );
+  localStorage.setItem("ethbal", String(ethbalance?.balance));
+  localStorage.setItem("ethbalusd", String(ethbalUsd));
+  localStorage.setItem(
+    "ethpriceusd",
+    String(ethereumInfo?.market_data?.current_price?.usd)
+  );
+  localStorage.setItem(
+    "polygonusdcbal",
+    String(polygonusdcbalance?.data?.balance)
+  );
+  localStorage.setItem("berausdcbal", String(berausdcbalance?.data?.balance));
+  localStorage.setItem(
+    "usdcusdprice",
+    String(usdcInfo?.market_data?.current_price?.usd)
+  );
+
+  useEffect(() => {
+    if (backButton.isSupported()) {
+      backButton.mount();
+    }
+
+    if (backButton.isVisible()) {
+      backButton.hide();
+    }
+
+    return () => {
+      backButton.unmount();
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      <AppBar />
+
+      <div id="hometab">
+        <WalletBalance
+          balancesLoading={
+            ethbalfetching ||
+            berabalfetching ||
+            polygonusdcfetching ||
+            berausdcbalfetching ||
+            unlockedtokensfetching ||
+            ethinfofetching ||
+            usdcinfofetching ||
+            berainfofetching ||
+            sphereusdcfetching
+          }
+          ethBalanceUsd={
+            Number(ethbalance?.balance) *
+              Number(ethereumInfo?.market_data?.current_price?.usd) || 0
+          }
+          beraBalanceUsd={
+            Number(berabalance?.data?.balance) *
+              Number(beraInfo?.market_data?.current_price?.usd) || 0
+          }
+          polygonUsdcBalanceUsd={
+            Number(polygonusdcbalance?.data?.balance) *
+              Number(usdcInfo?.market_data?.current_price?.usd) || 0
+          }
+          beraUsdcbalanceUsd={
+            Number(berausdcbalance?.data?.balance) *
+              Number(usdcInfo?.market_data?.current_price?.usd) || 0
+          }
+          sphereBalanceUsd={
+            Number(unlockedtokens?.amount) *
+              Number(sphereusdcrate?.data?.currentRate) *
+              Number(usdcInfo?.market_data?.current_price?.usd) || 0
+          }
+        />
+
+        <div className="actions">
+          <WalletAction
+            icon={<ArrowUpCircle color={colors.textprimary} />}
+            text="Send"
+            onclick={() => {}}
+          />
+
+          <WalletAction
+            icon={<PlusSolid color={colors.textprimary} />}
+            text="Buy"
+            onclick={() => {}}
+          />
+
+          <WalletAction
+            icon={<ArrowDownCircle color={colors.textprimary} />}
+            text="Deposit"
+            onclick={() => {}}
+          />
+        </div>
+
+        <AssetBalance
+          tokenLoading={
+            ethbalfetching ||
+            berabalfetching ||
+            polygonusdcfetching ||
+            berausdcbalfetching ||
+            unlockedtokensfetching ||
+            ethinfofetching ||
+            usdcinfofetching ||
+            berainfofetching ||
+            sphereusdcfetching
+          }
+          tokenImage={ethereumInfo?.image?.large as string}
+          tokenName={ethereumInfo?.name as string}
+          tokenSymbol={ethereumInfo?.symbol as string}
+          dayPriceChange={Number(
+            ethereumInfo?.market_data?.price_change_percentage_24h
+          )}
+          balance={Number(ethbalance?.balance)}
+          priceUsd={Number(ethereumInfo?.market_data?.current_price?.usd)}
+        />
+
+        <AssetBalance
+          tokenLoading={
+            ethbalfetching ||
+            berabalfetching ||
+            polygonusdcfetching ||
+            berausdcbalfetching ||
+            unlockedtokensfetching ||
+            ethinfofetching ||
+            usdcinfofetching ||
+            berainfofetching ||
+            sphereusdcfetching
+          }
+          tokenImage={beraInfo?.image?.large as string}
+          tokenName={beraInfo?.name as string}
+          tokenSymbol={beraInfo?.symbol as string}
+          dayPriceChange={Number(
+            beraInfo?.market_data?.price_change_percentage_24h
+          )}
+          balance={Number(berabalance?.data?.balance)}
+          priceUsd={Number(beraInfo?.market_data?.current_price?.usd)}
+        />
+
+        <AssetBalance
+          tokenLoading={
+            ethbalfetching ||
+            berabalfetching ||
+            polygonusdcfetching ||
+            berausdcbalfetching ||
+            unlockedtokensfetching ||
+            ethinfofetching ||
+            usdcinfofetching ||
+            berainfofetching ||
+            sphereusdcfetching
+          }
+          tokenImage={usdcInfo?.image?.large as string}
+          tokenName="USDC (Polygon)"
+          tokenSymbol={usdcInfo?.symbol as string}
+          dayPriceChange={Number(
+            usdcInfo?.market_data?.price_change_percentage_24h
+          )}
+          balance={Number(polygonusdcbalance?.data?.balance)}
+          priceUsd={Number(usdcInfo?.market_data?.current_price?.usd)}
+        />
+
+        <AssetBalance
+          tokenLoading={
+            ethbalfetching ||
+            berabalfetching ||
+            polygonusdcfetching ||
+            berausdcbalfetching ||
+            unlockedtokensfetching ||
+            ethinfofetching ||
+            usdcinfofetching ||
+            berainfofetching ||
+            sphereusdcfetching
+          }
+          tokenImage={usdcInfo?.image?.large as string}
+          tokenName="USDC (Berachain)"
+          tokenSymbol="USDC.e"
+          dayPriceChange={Number(
+            usdcInfo?.market_data?.price_change_percentage_24h
+          )}
+          balance={Number(berausdcbalance?.data?.balance)}
+          priceUsd={Number(usdcInfo?.market_data?.current_price?.usd)}
+        />
+      </div>
+    </Fragment>
+  );
 };
