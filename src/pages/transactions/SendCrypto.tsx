b9a67e1f9e3ccec0c4733553e304ef8002261178
mrgustaveh <mburuantony@outlook.com>
2025-05-08 15:04:00 +0300

    updated send crypto & send crypto options

--- src/pages/transactions/SendCrypto.tsx
+++ src/pages/transactions/SendCrypto.tsx
@@ -1,10 +1,12 @@
 import { JSX } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useBackButton } from "../../hooks/backbutton";
 import { useTabs } from "../../hooks/tabs";
+import "../../styles/pages/transactions/sendcrypto.scss";
 
 export default function SendCrypto(): JSX.Element {
   const navigate = useNavigate();
+  const { srccurrency, intent } = useParams();
   const { switchtab } = useTabs();
 
   const prev_page = localStorage.getItem("prev_page");
@@ -13,12 +15,8 @@ export default function SendCrypto(): JSX.Element {
     if (prev_page == null) {
       switchtab("home");
       navigate("/app");
-    } else if (prev_page === "rewards") {
-      switchtab("rewards");
-      navigate("/app");
-    } else if (prev_page === "send-options") {
-      switchtab("sendcrypto");
-      navigate("/app");
+    } else if (prev_page == "send-options") {
+      navigate(`/send-crypto-methods/${intent}`);
     } else {
       navigate(prev_page);
     }
@@ -26,5 +24,12 @@ export default function SendCrypto(): JSX.Element {
 
   useBackButton(goBack);
 
-  return <section id="sendcrypto">send crypto here</section>;
+  return (
+    <section id="sendcrypto">
+      send crypto to address here
+      {srccurrency}
+      <br />
+      {intent}
+    </section>
+  );
 }
