eb4c92236344e6c95db4add0f99b2fd3cda40d40
mrgustaveh <mburuantony@outlook.com>
2025-05-12 20:16:32 +0300

    updates & fixes

--- src/components/drawer/SendLendLink.tsx
+++ src/components/drawer/SendLendLink.tsx
@@ -2,7 +2,6 @@ import { JSX } from "react";
 import { openTelegramLink } from "@telegram-apps/sdk-react";
 import { useAppDrawer } from "../../hooks/drawer";
 import { useSnackbar } from "../../hooks/snackbar";
-import { SubmitButton } from "../global/Buttons";
 import { colors } from "../../constants";
 import { Copy, Telegram } from "../../assets/icons";
 import "../../styles/components/drawer/sendlendlink.scss";
@@ -19,18 +18,16 @@ export const SendLendLink = (): JSX.Element => {
   const onShareTg = () => {
     closeAppDrawer();
     openTelegramLink(
-      `https://t.me/share/url?url=${encodeURIComponent(
-        keyToshare as string
-      )}&text=Use this link to claim the ${secretPurpose} I shared with you on Sphere.`
+      `https://t.me/share/url?url=${encodeURIComponent(keyToshare as string)}`
     );
   };
 
   return (
     <div className="sendlendlink">
-      <p className="sendtitle">
-        Your Lend {secretPurpose} link was created successfully
-      </p>
+      <p className="sendtitle">Your link was created successfully</p>
+
       <p className="senddesc">Here's the link for you to share 🔗</p>
+
       <div className="actions">
         <button className="copylink" onClick={onCopyLink}>
           {keyToshare?.substring(0, 31) + "..."}
@@ -40,13 +37,12 @@ export const SendLendLink = (): JSX.Element => {
           </span>
         </button>
 
-        <SubmitButton
-          text="Share On Telegram"
-          icon={<Telegram width={18} height={18} color={colors.primary} />}
-          sxstyles={{ marginTop: "0.625rem", padding: "0.625rem" }}
-          onclick={onShareTg}
-        />
+        <button onClick={onShareTg} className="tg-link">
+          Share On Telegram
+          <Telegram width={18} height={15} color={colors.textprimary} />
+        </button>
       </div>
+
       <p className="mindesc">
         Please share this link with the intended receipient.
         {secretPurpose == "Key" && "You can still revoke access to this key."}
