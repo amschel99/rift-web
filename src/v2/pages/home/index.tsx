0bf6d5bb61f3effa4d48dcf6aeb864c71e6812e3
porkytheblack <porkytheblack@gmail.com>
2025-05-27 08:44:05 +0300

    fix: rest home tab height

--- src/v2/pages/home/index.tsx
+++ src/v2/pages/home/index.tsx
@@ -2,7 +2,7 @@
 
 export default function Home(){
     return (
-        <div className="w-full h-[300vh] flex flex-col items-center justify-center" >
+        <div className="w-full h-full flex flex-col items-center justify-center" >
             <p className="font-semibold text-accent-primary" >
                 Home
             </p>
