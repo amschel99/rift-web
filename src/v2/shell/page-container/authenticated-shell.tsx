503c08fb15191067a4b3154d828a7fd16b151e6f
mrgustaveh <mburuantony@outlook.com>
2025-06-20 11:22:13 +0300

    updated path

--- src/v2/shell/page-container/authenticated-shell.tsx
+++ src/v2/shell/page-container/authenticated-shell.tsx
@@ -16,11 +16,11 @@ export default function AuthenticatedShell(props: Props) {
     if (auth_token && address) {
       sphere.setBearerToken(auth_token);
     } else {
-      navigate("/");
+      navigate("/auth");
     }
   }, []);
   return (
-    <div className="w-screen h-screen flex flex-col items-center bg-app-background relative">
+    <div className="w-screen h-screen flex flex-col items-center relative">
       <div className="flex flex-col w-full flex-1 ">{children}</div>
       <div className="flex flex-row items-center justify-center px-5 bg-surface-subtle/60 backdrop-blur-1xl w-full shadow-2xl shadow-surface-subtle fixed bottom-0 pb-5">
         <BottomTabs />
