11c1d438ed8d9508640813367b87dad44108b0c8
amschel99 <6290.2020@students.ku.ac.ke>
2025-11-07 19:27:35 +0300

    fix:update rift version

--- vite.config.ts
+++ vite.config.ts
@@ -87,6 +87,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg}"],
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MB
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/api\./,
