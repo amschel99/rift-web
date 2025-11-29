22cdb5d06594c734a35d32ac84d0b6eff3da61da
amschel99 <6290.2020@students.ku.ac.ke>
2025-11-29 12:35:13 +0300

    fix:remove maintenance mode

--- src/components/MaintenanceMode.tsx
+++ src/components/MaintenanceMode.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //
-const MAINTENANCE_MODE = true; // Set to true to enable
+const MAINTENANCE_MODE = false; // Set to true to enable
 
 const MaintenanceMode: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   if (!MAINTENANCE_MODE) {
